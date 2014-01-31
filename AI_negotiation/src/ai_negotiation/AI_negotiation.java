8f1ffcfa94af38d8194b47becccad5e8f17fb40e
Benny <Benny@145.94.84.176>
2014-01-31 15:18:31 +0100

    test

---
+++ AI_negotiation/src/ai_negotiation/AI_negotiation.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ai_negotiation;
+
+/**
+ *
+ * @author Benny
+ */
+public class AI_negotiation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
