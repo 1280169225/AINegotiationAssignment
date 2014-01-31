f2aadca4a46d3aaf30002fddeb1afac86d533894
Benny <Benny@145.94.84.176>
2014-01-31 14:08:39 +0100

    first version acceptance strategy

---
+++ AI_negotiation/src/ai_negotiation/Group9AS.java
@@ -0,0 +1,50 @@
+
+package ai_negotiation;
+
+import java.util.HashMap;
+import java.util.List;
+import negotiator.BidHistory;
+import negotiator.Timeline;
+import negotiator.bidding.BidDetails;
+import negotiator.boaframework.AcceptanceStrategy;
+import negotiator.boaframework.Actions;
+import negotiator.boaframework.NegotiationSession;
+import negotiator.boaframework.OfferingStrategy;
+
+/**
+ *
+ * @author Benny
+ */
+public class Group9AS extends AcceptanceStrategy{
+
+    @Override
+    public void init(NegotiationSession negoSession, OfferingStrategy strat, HashMap<String, Double> parameters) throws Exception {
+        this.negotiationSession = negoSession;
+        this.offeringStrategy = strat;
+    }
+
+    @Override
+    public Actions determineAcceptability() {
+        BidHistory bh = this.negotiationSession.getOpponentBidHistory();
+        double myBidUtility = this.offeringStrategy.getNextBid().getMyUndiscountedUtil();
+        double oppBidUtility = bh.getLastBidDetails().getMyUndiscountedUtil();
+        double timeNorm = this.negotiationSession.getTime();
+        double currentTime = this.negotiationSession.getTimeline().getCurrentTime();
+        BidHistory currentBH = bh.filterBetweenTime(0, currentTime);
+        List<BidDetails> bids = currentBH.getHistory();
+        double meanUtility = currentBH.getAverageUtility();
+        double std = 0;
+        for(int i=0;i<bids.size();i++){
+            std = std + (bids.get(i).getMyUndiscountedUtil() - meanUtility);
+        }
+        std = std / bids.size();
+        double disFactor = this.negotiationSession.getDiscountFactor();
+        double bid = myBidUtility - (1 * std) * Math.pow(timeNorm,1-disFactor);
+                
+        if(oppBidUtility >= bid){
+            return Actions.Accept;
+        }
+        return Actions.Reject;
+    }
+
+}
